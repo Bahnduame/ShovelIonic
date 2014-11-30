ed8378eb4279366cd2c594e91a2b7a7e97e84a33
pmudahar <paul.mudahar@gmail.com>
2014-11-30 14:58:54 -0500

    added available toggle button

--- www/app/factories/userData.js
+++ www/app/factories/userData.js
@@ -8,7 +8,8 @@ angular.module('starter')
             status: 'none',
             ccInfo: null,
             type: '',
-            recipientID: ''
+            recipientID: '',
+            available: ''
     };
 
     return{
