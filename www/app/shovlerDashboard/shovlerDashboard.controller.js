f86ef0d8ddf49c8fc5f1077c7aefcc6d755a2b57
pmudahar <paul.mudahar@gmail.com>
2014-12-09 21:28:06 -0500

    toggle for availability works

--- www/app/shovlerDashboard/shovlerDashboard.controller.js
+++ www/app/shovlerDashboard/shovlerDashboard.controller.js
@@ -1,27 +1,30 @@
 angular.module('starter')
-.controller('ShovlerDashboardCtrl',function($scope, $state, userData, apptData, usSpinnerService){
-    $scope.appointments= {};
-    $scope.available = userData.getProperty('available');
+
+.controller('ShovlerDashboardCtrl',function($scope, $state, userData, apptData, usSpinnerService, $timeout){
+
+    $scope.available = { value: false };
+    
 
     // listen to changes in available, if it changes modify it in Firebase    
     var updateAvailable = function(){
         usSpinnerService.spin('spinner-1');   
 
         availableObj = {
-            available: $scope.available;
-        }
+            available: $scope.available.value
+        };
 
         ref.child('worker').child(userData.getID()).update(availableObj, function(err){
             usSpinnerService.stop('spinner-1');
         });
     }
+    
 
+    $scope.$watch("available.value", function(newVal, oldVal) { 
 
-    $scope.$watch($scope.available, function() { 
-        updateAvailable();
-    }, true);
-
-
+        if (newVal !== oldVal){
+            updateAvailable();
+        }
+    });
 
 
     ref.child('appointments').on('value',function(snapshot){
